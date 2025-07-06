d84d45a4850f4cd5bedbe4e8bb36b0a086db6584
yogita upadhyay <35460298+yogitaupadhyay@users.noreply.github.com>
2025-07-06 19:23:36 +0530

    Update outputs.tf

--- modules/app_engine/outputs.tf
+++ modules/app_engine/outputs.tf
@@ -16,31 +16,31 @@
 
 output "name" {
   description = "Unique name of the app, usually apps/{PROJECT_ID}."
-  value       = google_app_engine_application.main.name
+  value       = google_app_engine_application.app-example-yogita.name
 }
 
 output "url_dispatch_rule" {
   description = "A list of dispatch rule blocks. Each block has a domain, path, and service field."
-  value       = google_app_engine_application.main.url_dispatch_rule
+  value       = google_app_engine_application.app-example-yogita.url_dispatch_rule
 }
 
 output "code_bucket" {
   description = "The GCS bucket code is being stored in for this app."
-  value       = google_app_engine_application.main.code_bucket
+  value       = google_app_engine_application.app-example-yogita.code_bucket
 }
 
 output "default_hostname" {
   description = "The default hostname for this app."
-  value       = google_app_engine_application.main.default_hostname
+  value       = google_app_engine_application.app-example-yogita.default_hostname
 }
 
 output "default_bucket" {
   description = "The GCS bucket content is being stored in for this app."
-  value       = google_app_engine_application.main.default_bucket
+  value       = google_app_engine_application.app-example-yogita.default_bucket
 }
 
 output "location_id" {
   description = "The location app engine is serving from"
-  value       = google_app_engine_application.main.location_id
+  value       = google_app_engine_application.app-example-yogita.location_id
 }
 
